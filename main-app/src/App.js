6c66fc6128427582b9a778a81ef628de08fb4f74
Alex-Voinov <VoinovAlex1900@yandex.ru>
2023-04-28 21:08:51 +0300

    чистая маршрутизация

---
+++ main-app/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import StartPage from './pages/start_page/StartPage';
+import NonExistentPage from './pages/non_existent_page/NonExistentPage';
+import PersonalPage from './pages/personal_page/PersonalPage';
+import AuthorizationPage from './pages/authorization_page/AuthorizationPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<StartPage />} />
+        <Route path="*" element={<NonExistentPage />} />
+        <Route path="personal_account" element={<PersonalPage />} />
+        <Route path="authorization" element={<AuthorizationPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ main-app/src/pages/non_existent_page/NonExistentPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NonExistentPage = () => {
+    return (
+        <div>NonExistentPage</div>
+    )
+}
+
+export default NonExistentPage
