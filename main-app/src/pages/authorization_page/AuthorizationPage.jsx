00fbce3906f87e5795472e06ab0805117334235c
Alex-Voinov <VoinovAlex1900@yandex.ru>
2023-04-29 12:58:51 +0300

    Загружены все фавиконы, реализован переход на авт.

--- main-app/src/pages/authorization_page/AuthorizationPage.jsx
+++ main-app/src/pages/authorization_page/AuthorizationPage.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Matrix from '../../components/Matrix'
+import styles from './Overlay.module.css';
 
 const AuthorizationPage = () => {
+    document.body.classList.remove('blackout');
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 4000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
-        <Matrix />
+        <div>
+            {isLoading && <div className={styles.overlay}></div>}
+            <Matrix />
+        </div>
     )
 }
 
--- main-app/src/pages/start_page/StartPage.jsx
+++ main-app/src/pages/start_page/StartPage.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import { Link, useNavigate } from 'react-router-dom';
+import './style.css';
 const StartPage = () => {
+
+    const history = useNavigate();
+
+    const handleAuthClick = (e) => {
+        e.preventDefault();
+        document.body.classList.add('blackout');
+        setTimeout(function () {
+            history('/authorization', {replace: true});
+        }, 1000);
+    }
+
     return (
-        <div>StartPage</div>
+        <div>
+            <div>StartPage</div>
+            <Link to="/authorization" onClick={handleAuthClick}>authorization</Link>
+        </div>
     )
 }
 
-export default StartPage
+export default StartPage;
