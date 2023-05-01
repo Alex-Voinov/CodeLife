162f13c133c963fa7426556b19da0aa0b56896a2
Alex-Voinov <VoinovAlex1900@yandex.ru>
2023-05-01 13:50:02 +0300

    Сделаны функции для работы с 404

--- main-app/src/pages/non_existent_page/NonExistentPage.jsx
+++ main-app/src/pages/non_existent_page/NonExistentPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './style.css';
 import AnimatedStripe from '../../components/AnimatedStripe';
 import DesignButton from '../../components/UI/Button/DesignButton';
@@ -7,7 +7,30 @@ import {useNavigate } from 'react-router-dom';
 const NonExistentPage = () => {
 
     const history = useNavigate();
+    const [styleObject, setStyleObject] = useState(null);
     
+    const setupOptionAnimatedStripe = () => {
+        const error404_parrent = document.getElementsByClassName('local_midle')[0];
+        const topValue = parseInt(error404_parrent.offsetTop);
+        const error404 = error404_parrent.children[1];
+        const error404_styles = window.getComputedStyle(error404);
+        const fontSizeValue = parseInt(error404_styles.getPropertyValue('font-size'));
+        const styleObject = {
+            top: topValue +'px',
+            left: '-62vw',
+            height: fontSizeValue + 'px'
+        };
+        setStyleObject(styleObject);
+    }
+
+    const goToPage = link => {
+        history(link, {replace: true});
+        window.removeEventListener('load', setupOptionAnimatedStripe);
+        window.removeEventListener('resize', setupOptionAnimatedStripe);
+    }
+
+    window.addEventListener('load', setupOptionAnimatedStripe);
+    window.addEventListener('resize', setupOptionAnimatedStripe);
 
     return (
         <div className='main_background'>
@@ -15,11 +38,11 @@ const NonExistentPage = () => {
                 <div className='error_ntf '>Страница не найдена</div>
             </div>  
             <div className='local_midle'>
-                <AnimatedStripe style={{top:'3.5vh', left: '-62vw'}}/>
+                <AnimatedStripe style={styleObject}/>
                 <div className="error_code">404</div>
                 <div className='button_block'>
-                    <DesignButton onClick={()=>{history('', {replace: true});}}>На главную</DesignButton>
-                    <DesignButton>В личный кабинет</DesignButton>
+                    <DesignButton onClick={()=>{goToPage('')}}>На главную</DesignButton>
+                    <DesignButton onClick={()=>{goToPage('personal_account')}}>В личный кабинет</DesignButton>
                 </div>
                 <div style={{display: 'flex'}}>
                 <div className='company_title glow_element'>Code Live</div>
