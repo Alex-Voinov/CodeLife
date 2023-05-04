86e9731f4b01b4608c610c437e8596e4f4b00619
Alex-Voinov <VoinovAlex1900@yandex.ru>
2023-05-04 21:52:00 +0300

    Начата разработка лс

---
+++ main-app/src/components/PA/Top_menu/TopMenu.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styles from './TopMenu.module.css'
+import CompanyLogo from '../../Decorations/CL_logo/CompanyLogo'
+
+const TopMenu = ({outLink}) => {
+
+    const logoStyle = {
+        width: 'var(--IC_logo_width)',
+        height: 'var(--IC_logo_height)',
+        borderRadius: 'var(--IC_logo_border_radius)',
+        borderWidth: 'var(--IC_logo_border_width)',
+        marginLeft: 'var(--IC_logo_ml)',
+        fontSize: 'var(--IC_logo_font_size)',
+        lineHeight: '31px',
+    }
+    return (
+        <div className={styles.header_menu}>
+            <CompanyLogo 
+                style={logoStyle}
+            />
+            <div className={styles.IC_title}>
+                Ikigai Creative
+            </div>
+            <div className={styles.header_avatar}></div>
+            <div className={styles.header_right}>
+                <img className={styles.notific} src="/static/img/icons/notific_2.png" alt=""/>
+                    <div className={styles.header_right_content}>
+                        <a className={styles.chenge_lang} href="#">RU</a>
+                        <a onClick={outLink} className={styles.exit_link} href="#">Выйти</a>
+                    </div>
+            </div>
+        </div>
+    )
+}
+
+export default TopMenu
--- main-app/src/pages/personal_page/PersonalPage.jsx
+++ main-app/src/pages/personal_page/PersonalPage.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import {useNavigate } from 'react-router-dom';
 import generateFunctionTransfer from '../../utils/animatedBacground';
+import TopMenu from '../../components/PA/Top_menu/TopMenu';
+
 const PersonalPage = () => {
+    
     const history = useNavigate();
-    const [goMain] = generateFunctionTransfer(history, ['goMain'])
+    const [goMain] = generateFunctionTransfer(history, ['/'])
 
     return (
-        <div>PersonalPage</div>
+        <>
+            <TopMenu outLink={goMain}/>
+        </>
     )
 }
 
