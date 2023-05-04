8b00f56bc69bd9a7f538973a35dde5eea79620a1
Alex-Voinov <VoinovAlex1900@yandex.ru>
2023-05-04 15:09:59 +0300

    Пидорас очко потряс

--- main-app/src/pages/personal_page/PersonalPage.jsx
+++ main-app/src/pages/personal_page/PersonalPage.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
-
+import {useNavigate } from 'react-router-dom';
+import generateFunctionTransfer from '../../utils/animatedBacground';
 const PersonalPage = () => {
+    const history = useNavigate();
+    const [goMain] = generateFunctionTransfer(history, ['goMain'])
+
     return (
         <div>PersonalPage</div>
     )
