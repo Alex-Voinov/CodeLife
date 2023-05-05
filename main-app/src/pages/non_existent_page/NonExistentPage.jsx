a2c4cf645eb6e9069e972e86a91ec9efa2d039fd
Andrey <dobrenko2000@mail.ru>
2023-05-05 16:54:19 +0400

    Добавил

--- main-app/src/pages/non_existent_page/NonExistentPage.jsx
+++ main-app/src/pages/non_existent_page/NonExistentPage.jsx
@@ -13,7 +13,7 @@ const NonExistentPage = () => {
 
     return (
         <div className='main_background'>
-            <div className='company_title_mobile glow_element'>Code Live</div>
+            <CompanyTitle id='company_title_mobile_404'/>
             <img src='/img/girl_404.png' className='girl_image blur_animation' alt="perplexed girl" />
             <div className='button_block_mobile'>
                         <DesignButton onClick={goMain} id='button_404_gomain'>Главная страница</DesignButton>
@@ -30,7 +30,7 @@ const NonExistentPage = () => {
                         <DesignButton onClick={goMain} id='button_404_gomain'>Главная страница</DesignButton>
                         <DesignButton onClick={goLC} id='button_404_goLC'>Личный кабинет</DesignButton>
                     </div>
-                    <CompanyTitle/>
+                    <CompanyTitle id='company_title_404'/>
                 </div>
                 <div className='local_header_mobile'>
                     <div className='error_ntf '>Страница<br/>не найдена</div>
